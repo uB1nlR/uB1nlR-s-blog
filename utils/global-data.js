8c18a67ca856d52a1f4d72a27fa346b9a9589be2
uB1nlR <73697155+uB1nlR@users.noreply.github.com>
2024-02-19 19:50:15 +0800

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'uB1nlR's blog.';
+    : 'uB1nlRsblog.';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Welcome to uB1nlR's blog!';
+    : 'Welcome to uB1nlRsblog!';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'uB1nlR 2024';
+    : 'uB1nlR2024';
 
   return {
     name,
